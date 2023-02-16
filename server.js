a80cac8ec0aed76bc5dd9a8c406f9b759a066b58
Grayson Hoff <the.graysonhoff@gmail.com>
2023-02-16 17:33:00 -0600

    tests complete

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ let newFolderNameCounter = 1
 
 const newFolderName = function (folders) {
   for (let i = 0; i < folders.length; i++) {
-    console.log(folders[i])
     newFolderNameCounter++
   }
   return 'New Folder ' + '(' + newFolderNameCounter + ')'
 }
-console.log(newFolderName(['New Folder']))
+// console.log(newFolderName(['New Folder']))
+// console.log(newFolderName(['New Folder', 'New Folder (2)']))
+console.log(newFolderName(['New Folder', 'New Folder (2)', 'New Folder (3)']))
